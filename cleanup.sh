2b763ed229b8e98f60e7935da93eb3b8ecb1fd2a
Sean McKenna <seanmck@microsoft.com>
2024-03-14 15:12:46 +0000

    Initial commit

---
+++ cleanup.sh
@@ -0,0 +1,7 @@
+kubectl delete deploy inflate-large-spot
+kubectl delete deploy inflate-med
+kubectl delete deploy inflate-small-gpu
+kubectl delete deploy inflate-large
+
+kubectl delete nodepool nodepool2
+kubectl delete nodepool nodepool3
